11f0302cfc6b57eee28f2dd9b73cbe1508dcf7b3
luis190508 <luigi.ditillo@capellinisauro.edu.it>
2026-05-25 11:23:00 +0000

    stampa 3 volte nome

--- helloworld.java
+++ helloworld.java
@@ -5,6 +5,9 @@ public class helloworld
     static void main(String[]args)
     {
         System.out.println("Hello Luigi Ditillo");
+        System.out.println("Hello Luigi Ditillo");
+        System.out.println("Hello Luigi Ditillo");
+        
     }
 
 }
